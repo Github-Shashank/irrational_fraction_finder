028feddf2707d425ec2556928f4a8b0948376f6a
Shashank <116636799+Github-Shashank@users.noreply.github.com>
2025-06-04 16:59:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="irrational_finder",
-    version="0.1",
-    author="Shashank Singh Patel",
-    description="Find rational approximations of irrational numbers",
+    name='irrational_fraction_finder',
+    version='0.1.0',
+    author='Shashank Singh Patel',
+    description='Approximate irrational numbers with rational fractions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Github-Shashank/irrational_fraction_finder',
     packages=find_packages(),
-    python_requires=">=3.6",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
