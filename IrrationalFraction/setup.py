646fab64f6be84ccc599bb7785d439935fd01e64
Github-Shashank <116636799+Github-Shashank@users.noreply.github.com>
2025-06-04 16:26:47 +0530

    Initial Commit

---
+++ IrrationalFraction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="irrational_finder",
+    version="0.1",
+    author="Shashank Singh Patel",
+    description="Find rational approximations of irrational numbers",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
